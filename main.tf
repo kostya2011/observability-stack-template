80d521ebfba1f278e901014dab6d38f6fc4ac941
Yahodzinskyi Konstantin <kartograph9@gmail.com>
2022-12-03 19:23:47 +0200

    Add helm tf provider, create monitoring helm release within tf

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 locals {
+  kube_config = {
+    config_path    = "~/.kube/config"
+    config_context = "minikube"
+  }
   namespaces = ["demo-apps", "demo-monitoring"]
+  helm_releases = {
+    monitoring = {
+      chart     = "./monitoring-helm"
+      namespace = "demo-monitoring"
+      atomic    = true
+      values = [
+        file("./monitoring-helm/values.yaml")
+      ]
+    }
+  }
 }
 
 terraform {
@@ -9,8 +23,15 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+  config_path    = local.kube_config.config_path
+  config_context = local.kube_config.config_context
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = local.kube_config.config_path
+    config_context = local.kube_config.config_context
+  }
 }
 
 resource "kubernetes_namespace" "this" {
@@ -23,4 +44,31 @@ resource "kubernetes_namespace" "this" {
 
     name = each.key
   }
+}
+
+# Helm releases
+resource "helm_release" "this" {
+  for_each = local.helm_releases
+
+  name       = each.key
+  repository = try(each.value.repository, null)
+  chart      = each.value.chart
+  version    = try(each.value.version, null)
+
+  values    = try(each.value.values, null)
+  namespace = try(each.value.namespace, "default")
+  verify    = try(each.value.verify, false)
+  atomic    = try(each.value.atomic, false)
+
+  dynamic "set" {
+    for_each = try(each.value.override_values, {})
+    content {
+      name  = set.value["name"]
+      value = set.value["value"]
+    }
+  }
+
+  depends_on = [
+    kubernetes_namespace.this
+  ]
 }

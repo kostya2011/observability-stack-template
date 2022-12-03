efaf7315cbe3499c7f1951b2b3420afd7546ce8c
Yahodzinskyi Konstantin <kartograph9@gmail.com>
2022-12-03 19:48:57 +0200

    Separate configs from main.tf (#5)

---
+++ config.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
+locals {
+  kube_config = {
+    config_path    = "~/.kube/config"
+    config_context = "minikube"
+  }
+  namespaces = ["demo-apps", "demo-monitoring"]
+  helm_releases = {
+    monitoring = {
+      chart           = "./monitoring-helm"
+      namespace       = "demo-monitoring"
+      atomic          = true
+      cleanup_on_fail = true
+      values = [
+        file("./monitoring-helm/values.yaml")
+      ]
+    }
+    echo-server = {
+      repository      = "https://ealenn.github.io/charts"
+      chart           = "echo-server"
+      version         = "0.5.0"
+      atomic          = true
+      cleanup_on_fail = true
+      namespace       = "demo-apps"
+    }
+  }
+}
+
+
---
+++ providers.tf
@@ -0,0 +1,11 @@
+provider "kubernetes" {
+  config_path    = local.kube_config.config_path
+  config_context = local.kube_config.config_context
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = local.kube_config.config_path
+    config_context = local.kube_config.config_context
+  }
+}

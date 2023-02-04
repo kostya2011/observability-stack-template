c80e0a55d85db4061a011b4f0207c63c8979eb11
Yahodzinskyi Konstantin <kartograph9@gmail.com>
2023-02-04 22:10:48 +0200

    Enable minio

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "minio_creds" {
+  value = {
+    name     = "root"
+    password = random_string.minio_password.result
+  }
+}
--- versions.tf
+++ versions.tf
@@ -8,5 +8,9 @@ terraform {
       source  = "hashicorp/helm"
       version = "~>2.7"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.4.3"
+    }
   }
 }

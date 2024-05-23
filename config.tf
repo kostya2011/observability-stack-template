8c28adc9e852d4287c2d462dee5ce26d653f01eb
test commit <kyahodzinskyi@local>
2024-05-23 16:22:38 +0300

    add fix terraform install

--- config.tf
+++ config.tf
@@ -9,11 +9,7 @@ locals {
     config_path    = "~/.kube/config"
     config_context = "minikube"
   }
-  namespaces = [
-    "demo-apps",
-    "demo-monitoring",
-    "demo-loki"
-  ]
+  namespaces = ["demo-apps", "demo-monitoring", "demo-loki"]
   helm_releases_tmpl = {
     monitoring = {
       enabled         = var.monitoring_helm_enabled
